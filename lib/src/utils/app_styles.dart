2cb425fb359422e881607a9c6c46c3922cd3ad0e
José Pazmiño <pepe.paz0399@gmail.com>
2023-06-02 16:24:04 -0500

    Add magenta color

--- lib/src/utils/app_styles.dart
+++ lib/src/utils/app_styles.dart
@@ -9,13 +9,13 @@ class Styles {
   static Color transparent = const Color.fromARGB(0, 0, 0, 0);
   static Color bottonNBarBorderColor = const Color.fromARGB(255, 176, 217, 255);
   static Color white = const Color.fromARGB(255, 255, 255, 255);
+  static Color magenta  = Color.fromARGB(255, 224, 17, 96);
 
   //gradientContainer
   static Color gradient_0 = const Color.fromARGB(255, 26, 33, 63);
   static Color gradient_1 = const Color.fromARGB(180, 26, 33, 63);
   static Color gradient_2 = const Color.fromARGB(97, 26, 33, 63);
   static Color gradient_3 = const Color.fromARGB(0, 100, 100, 100);
-  
 
   static TextStyle textStyleTitle = TextStyle(
     fontSize: 25,
