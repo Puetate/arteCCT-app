67efd573a676a5102a45f62af9294c52d8f43e38
José Pazmiño <pepe.paz0399@gmail.com>
2023-06-26 01:04:01 -0500

    Create ui search tab

--- lib/src/screens/home/tabs/search_tab/search_tab.dart
+++ lib/src/screens/home/tabs/search_tab/search_tab.dart
@@ -1,4 +1,7 @@
+import 'package:arte_ctt_app/src/screens/components/card_picture.dart';
+import 'package:arte_ctt_app/src/screens/components/search_input.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class SearchTab extends StatefulWidget {
   const SearchTab({super.key});
@@ -10,8 +13,33 @@ class SearchTab extends StatefulWidget {
 class _SearchTabState extends State<SearchTab> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("Buscador")),
+    return ListView(
+      children:[
+         Column(
+        children: [
+          const Gap(60),
+          SearchInput(
+            onSearchChange: (value) {
+              print("search value ${value}");
+            },
+          ),
+          const Gap(60),
+          GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 2,
+            children: const [
+              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
+              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
+              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
+              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
+              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
+              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
+          
+            ],
+          )
+        ],
+      ),
+      ]
     );
   }
 }
