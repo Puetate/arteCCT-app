68a9f50ccec403cf68297aefe0896fbcdc246cb5
Pablo <84303588+Puetate@users.noreply.github.com>
2023-07-07 22:13:03 -0500

    add proxy and Provider mangerProxy

--- lib/src/data/datasource/api_repository_impl.dart
+++ lib/src/data/datasource/api_repository_impl.dart
@@ -1,41 +1,40 @@
-import 'package:arte_ctt_app/src/data/in_memory_agenda.dart';
-import 'package:arte_ctt_app/src/data/in_memory_pictures.dart';
 import 'package:arte_ctt_app/src/domain/api/casa_cultura_tungurahua_api.dart';
 import 'package:arte_ctt_app/src/domain/models/agenda.dart';
 import 'package:arte_ctt_app/src/domain/models/picture.dart';
 import 'package:arte_ctt_app/src/domain/repository/api_repository.dart';
-import 'package:flutter/material.dart';
 
 class ApiRepositoryImpl extends ApiRepositoryInterface {
   @override
-  Future<List<Picture>> getPictures() async {
-    var res = await Api.httpGet('/artworks');
+  Future<List<Picture>> getPictures(value) async {
+    var res = await Api.httpGet('/artworks/search/$value');
     List<Picture> pictures = (res as List)
         .map(
           (p) => Picture.fromJson(p),
         )
         .toList();
-    print(pictures);
     return pictures;
   }
 
   @override
   Future<List<Agenda>> getAgenda() async {
-    await Future.delayed(const Duration(seconds: 1));
-    return agendaItems;
+    var res = await Api.httpGet('/agenda');
+    if (res == null) return List<Agenda>.empty();
+    List<Agenda> agenda = (res as List).map((a) => Agenda.fromJson(a)).toList();
+    return agenda;
   }
 
   @override
-  Future<Picture> getPicture(String id) async {
+  Future<Picture?> getPicture(String id) async {
     var res = await Api.httpGet('/artworks/$id');
+    if (res == null) return null;
     Picture picture = Picture.fromJson(res);
-    print(picture);
     return picture;
   }
 
   @override
-  Future<List<Picture>> getFeaturedPictures() async {
+  Future<List<Picture>> getFeaturedPictures(needReset) async {
     var res = await Api.httpGet('/artworks/featured');
+    if (res == null) return List<Picture>.empty();
     List<Picture> featuredPictures = (res as List)
         .map(
           (p) => Picture.fromJson(p),
--- lib/src/domain/repository/api_repository.dart
+++ lib/src/domain/repository/api_repository.dart
@@ -2,8 +2,8 @@ import 'package:arte_ctt_app/src/domain/models/agenda.dart';
 import 'package:arte_ctt_app/src/domain/models/picture.dart';
 
 abstract class ApiRepositoryInterface {
-  Future<List<Picture>> getPictures();
-  Future<List<Picture>> getFeaturedPictures();
+  Future<List<Picture>> getPictures(String value);
+  Future<List<Picture>> getFeaturedPictures(bool needReset);
   Future<List<Agenda>> getAgenda();
-  Future<Picture> getPicture(String id);
+  Future<Picture?> getPicture(String id);
 }
