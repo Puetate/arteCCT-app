b1e5105951e1292bb5ff86effa68b490837c665b
José Pazmiño <pepe.paz0399@gmail.com>
2023-07-07 21:36:40 -0500

    Add parameters in CardPicture

--- lib/src/screens/home/tabs/search_tab/search_tab.dart
+++ lib/src/screens/home/tabs/search_tab/search_tab.dart
@@ -1,5 +1,6 @@
 import 'package:arte_ctt_app/src/screens/components/card_picture.dart';
 import 'package:arte_ctt_app/src/screens/components/search_input.dart';
+import 'package:arte_ctt_app/src/utils/app_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -13,33 +14,38 @@ class SearchTab extends StatefulWidget {
 class _SearchTabState extends State<SearchTab> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children:[
-         Column(
+      final Size size = AppLayout.getSize(context);
+    return Column(
         children: [
-          const Gap(60),
-          SearchInput(
-            onSearchChange: (value) {
-              print("search value ${value}");
-            },
-          ),
-          const Gap(60),
-          GridView.count(
-            shrinkWrap: true,
-            crossAxisCount: 2,
-            children: const [
-              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
-              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
-              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
-              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
-              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
-              Padding(padding: EdgeInsets.all(8),child: CardPicture(),),
+     const Gap(60),
+     SearchInput(
+       onSearchChange: (value) {
+         print("search value ${value}");
+       },
+     ),
+     const Gap(60),
+     Container(
+      height: size.height - 290,
+       child: GridView.count(
+         shrinkWrap: true,
+         crossAxisCount: 2,
+         scrollDirection: Axis.vertical,
+         children: const [
+           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
+           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
+           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
+           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
+           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
+           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
+           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
+           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
+           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
+           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
           
-            ],
-          )
+         ],
+       ),
+     )
         ],
-      ),
-      ]
-    );
+      );
   }
 }
