85c085372c39ba024b5e8f8509fa0fcd8ffb53ad
José Pazmiño <pepe.paz0399@gmail.com>
2023-07-08 01:46:29 -0500

    Fix overflow error

--- lib/src/screens/home/tabs/search_tab/search_tab.dart
+++ lib/src/screens/home/tabs/search_tab/search_tab.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
+import 'package:arte_ctt_app/src/data/datasource/api_repository_impl.dart';
+import 'package:arte_ctt_app/src/domain/models/picture.dart';
 import 'package:arte_ctt_app/src/screens/components/card_picture.dart';
 import 'package:arte_ctt_app/src/screens/components/search_input.dart';
 import 'package:arte_ctt_app/src/utils/app_layout.dart';
+import 'package:arte_ctt_app/src/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -12,40 +17,92 @@ class SearchTab extends StatefulWidget {
 }
 
 class _SearchTabState extends State<SearchTab> {
+  late ApiRepositoryImpl service;
+  String title = "Buscar Obras";
+  String _query = "";
+  Timer? _debounce;
+  @override
+  void initState() {
+    super.initState();
+    service = ApiRepositoryImpl();
+  }
+
+  @override
+  void dispose() {
+    _debounce?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-      final Size size = AppLayout.getSize(context);
-    return Column(
+    final Size size = AppLayout.getSize(context);
+
+    return SingleChildScrollView(
+      physics: const NeverScrollableScrollPhysics(),
+      child: Column(
         children: [
-     const Gap(60),
-     SearchInput(
-       onSearchChange: (value) {
-         print("search value ${value}");
-       },
-     ),
-     const Gap(60),
-     Container(
-      height: size.height - 290,
-       child: GridView.count(
-         shrinkWrap: true,
-         crossAxisCount: 2,
-         scrollDirection: Axis.vertical,
-         children: const [
-           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
-           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
-           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
-           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
-           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
-           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
-           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
-           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
-           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
-           Padding(padding: EdgeInsets.all(8),child: CardPicture(urlImage:  'https://culturatungurahua.com/wp-content/uploads/2022/12/cuadro-24.gif',title: "Caballos briosos",),),
-          
-         ],
-       ),
-     )
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 15),
+            child: Text(
+              title,
+              style: Styles.textStyleTitle,
+            ),
+          ),
+          SearchInput(
+            onSearchChange: (value) {
+              if (_debounce?.isActive ?? false) _debounce?.cancel();
+              _debounce = Timer(const Duration(milliseconds: 300), () {
+                setState(() {
+                  _query = value;
+                });
+              });
+            },
+          ),
+          const Gap(10),
+          SizedBox(
+            height: size.height - 240,
+            child: FutureBuilder(
+              future: service.getPictures(_query),
+              builder: (context, snapshot) {
+                if (snapshot.hasData &&
+                    snapshot.data!.isNotEmpty &&
+                    snapshot.connectionState == ConnectionState.done) {
+                  List<Picture> pictures = snapshot.data!;
+                  return CustomScrollView(
+                    slivers: [
+                      SliverGrid(
+                          delegate: SliverChildBuilderDelegate(
+                              childCount: pictures.length, (context, index) {
+                            Picture picture = pictures[index];
+
+                            return CardPicture(
+                              picture: picture,
+                            );
+                          }),
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisExtent: 200,
+                            crossAxisSpacing: 5,
+                            mainAxisSpacing: 5,
+                          )),
+                    ],
+                  );
+                } else {
+                  return SizedBox(
+                    height: size.height,
+                    child: Center(
+                      child: CircularProgressIndicator(
+                        color: Styles.white,
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
         ],
-      );
+      ),
+    );
   }
 }
